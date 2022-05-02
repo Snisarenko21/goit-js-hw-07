beace463b796c164a6b210197d399a1d55b5695d
Snisarenko21 <sepalena@gmail.com>
2022-05-02 14:35:18 +0300

    на проверку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,51 +2,31 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-const refs = document.querySelector('.gallery');
+const refs = document.querySelector(".gallery");
 
-const createListImageEl = galleryItems.map(galleryItem =>
-`<div class="gallery__item">
-<a class="gallery__link" href="${galleryItem.original}">
-<img class="gallery__image" src = "${galleryItem.preview}" data-source="${galleryItem.original}" alt = "${galleryItem.description}"/>
-</a></div>`)
-.join("");
+const listEl = createListEl(galleryItems);
 
-// --------------рендер разметки-------------
-
-refs.insertAdjacentHTML("beforeend", createListImageEl);
-
-// -------------------вешаю слушатель событий на див с создаными элементами-----------
-refs.addEventListener('click', onGalleryItemsClick);
- 
-// ------------колбек - функция - обработчик событый -------- 
-function onGalleryItemsClick(event) {
-  // -----------запрет для браузера на действия по умолчанию (чтоб не открывал картинку по ссылке )------------
-  event.preventDefault();
-  
-  // --------проверяю куда попадает мой клик - если не в картинку, то выходим и ничего не делаем)-------------
-  if (event.target.nodeName != 'IMG') {
-    return;
-  } // -------иначе, используя библиотеку бейсиклайтбокс, открываем оригинальное изображение в модальном окне-----------
-  else {
-    const instance = basicLightbox.create(`<img width="1400" height="900" src= "${event.target.dataset.source}">`, {
-
-      // ---добавляю опции при открытом модальном окне-----------
-      onShow: (instance) => {
-        // ---колбек-функция -обработчик события ---
-        instance.onInstanceclick = function (e) {
-         
-          if (e.code === "Escape") {
-            instance.close();
-          }
-        }
-        // ---добавляю слушательсобытий клавиатуры на кнопку эскейп при открытом модальном окне-----------
-        document.addEventListener("keydown", instance.onInstanceclick);
-      },
-      // -------снимаю слушателя ссобытий после закрытия модального окна------
-      onClose: (instance) => {
-        document.removeEventListener("keydown", instance.onInstanceclick);
-      }
-    })
-.show();
-  }
+function createListEl(galleryItems) {
+  return galleryItems.map(galleryItem =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${galleryItem.original}">
+    <img class="gallery__image" src = "${galleryItem.preview}" data-source="${galleryItem.original}" alt = "${galleryItem.description}"/>
+    </a>
+    </div>`)
+    .join("");
 }
+refs.insertAdjacentHTML("beforeend", listEl);
+
+refs.addEventListener(`click`, (event)=> {
+
+event.preventDefault(); 
+
+const imgElement = event.target.classList.contains(`gallery__image`)
+if(!imgElement){
+  return
+}
+
+const instance = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`).show();
+
+});
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,22 +3,26 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const galleryListEl = document.querySelector('.gallery');
-console.log(galleryListEl);
 
-const createListImageEl = galleryItems.map(galleryItem =>
-`<a class="gallery__item" href="${galleryItem.original}">
-<img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/>
-</a>`)
-.join("");
+const listEl = createListEl(galleryItems);
 
+function createListEl(galleryItems) {
 
-galleryListEl.insertAdjacentHTML("beforeend", createListImageEl);
+    return galleryItems.map(galleryItem =>
+   `<a class="gallery__item" href="${galleryItem.original}">
+   <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/>
+   </a>`)
+   .join("");
+}
+
+galleryListEl.insertAdjacentHTML("beforeend", listEl);
 
 let lightbox = new SimpleLightbox('.gallery a', {});
 
-lightbox.options.captionsData = `alt`;
-lightbox.options.captionPosition = 'bottom';
-lightbox.options.captionDelay = 250;
+    lightbox.options.captionsData = `alt`;
+    lightbox.options.captionPosition = 'bottom';
+    lightbox.options.captionDelay = 250;
+
 
 
 
